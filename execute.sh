3567ef9f2b2eef7b23af24c336a725111742113c
Jaime Parra Jr <jparra@us.ibm.com>
2015-11-05 11:55:12 -0600

    Make dra_simple _test similar to simple tester type. Start executing the grunt idra commands.

--- execute.sh
+++ execute.sh
@@ -46,67 +46,67 @@ set +x
 #debugme echo "SAUCE_ACCESS_KEY: ${SAUCE_ACCESS_KEY}"
 #debugme echo "USER_ID: ${SAUCE_USERNAME}"
 
-cmd_choice=$CMD_CHOICE
+#cmd_choice=$CMD_CHOICE
 
-function execute { 
-    eval $cmd_choice
-    RESULT=$?
-    echo "$cmd_choice"
-    echo "$RESULT"
-#    ${EXT_DIR}/sauce.py
-#    PY_RES=$?
-    
-#    if [ "${DOWNLOAD_ASSETS}" == true ]; then
-#        echo "zipping files"
-#        zip -q selenium_logs.zip selenium-server-*
-#        zip -q videos.zip video-*
-#        mv selenium_logs.zip ${ARCHIVE_DIR}
-#        mv videos.zip ${ARCHIVE_DIR}
+#function execute { 
+#    eval $cmd_choice
+#    RESULT=$?
+#    echo "$cmd_choice"
+#    echo "$RESULT"
+##    ${EXT_DIR}/sauce.py
+##    PY_RES=$?
+#    
+##    if [ "${DOWNLOAD_ASSETS}" == true ]; then
+##        echo "zipping files"
+##        zip -q selenium_logs.zip selenium-server-*
+##        zip -q videos.zip video-*
+##        mv selenium_logs.zip ${ARCHIVE_DIR}
+##        mv videos.zip ${ARCHIVE_DIR}
+##    fi
+#    
+#    if [ "${DRA}" == true ]; then
+#        #insert DRA commands here
+#        npm install grunt
+#        npm install grunt-cli
+#        npm install grunt-idra
+#        grunt --gruntfile=node_modules/grunt-idra/idra.js -eventType=SaucelabFvtTest -file=job_data_collection.json -deployAnalyticsServer="$DA_SERVER"
 #    fi
-    
-    if [ "${DRA}" == true ]; then
-        #insert DRA commands here
-        npm install grunt
-        npm install grunt-cli
-        npm install grunt-idra
-        grunt --gruntfile=node_modules/grunt-idra/idra.js -eventType=SaucelabFvtTest -file=job_data_collection.json -deployAnalyticsServer="$DA_SERVER"
-    fi
-    
-#    mv job_data_collection.json ${ARCHIVE_DIR}
-    
-#    if [ $RESULT -ne 0 ] || [ $PY_RES -ne 0 ]; then
+#    
+##    mv job_data_collection.json ${ARCHIVE_DIR}
+#    
+##    if [ $RESULT -ne 0 ] || [ $PY_RES -ne 0 ]; then
+##        exit 1
+##    fi
+#    if [ $RESULT -ne 0 ]; then
 #        exit 1
 #    fi
-    if [ $RESULT -ne 0 ]; then
-        exit 1
-    fi
-}
+#}
 
-if [[ $cmd_choice == "npm test" ]] || [[ $cmd_choice == "grunt test" ]] || [[ $cmd_choice == "grunt" ]]; then
-    npm install
-    execute
-fi
-if [[ $cmd_choice == "ant test" ]] || [[ $cmd_choice == "mvn test" ]]; then
-    execute
-fi
-if [[ $cmd_choice == "custom" ]]; then
-    custom_cmd
-fi
-if [[ $cmd_choice == " " ]] || [[ -z "$cmd_choice" ]]; then
-    ${EXT_DIR}/auto-detect.py
-    AUTO_RESULT=$?
-    if [ $AUTO_RESULT -ne 0 ]; then
-        echo "Running grunt test..."
-        cmd_choice="grunt test"
-        npm install
-        execute
-    else
-        echo "Running npm test..."
-        cmd_choice="npm test"
-        npm install
-        execute
-    fi
-fi
+#if [[ $cmd_choice == "npm test" ]] || [[ $cmd_choice == "grunt test" ]] || [[ $cmd_choice == "grunt" ]]; then
+#    npm install
+#    execute
+#fi
+#if [[ $cmd_choice == "ant test" ]] || [[ $cmd_choice == "mvn test" ]]; then
+#    execute
+#fi
+#if [[ $cmd_choice == "custom" ]]; then
+#    custom_cmd
+#fi
+#if [[ $cmd_choice == " " ]] || [[ -z "$cmd_choice" ]]; then
+#    ${EXT_DIR}/auto-detect.py
+#    AUTO_RESULT=$?
+#    if [ $AUTO_RESULT -ne 0 ]; then
+#        echo "Running grunt test..."
+#        cmd_choice="grunt test"
+#        npm install
+#        execute
+#    else
+#        echo "Running npm test..."
+#        cmd_choice="npm test"
+#        npm install
+#        execute
+#    fi
+#fi
 
 function dra_logger {
 #    npm install grunt
@@ -166,12 +166,18 @@ function dra_commands {
         fi
         
         echo -e "\tFINAL dra_grunt_command: $dra_grunt_command"
+        echo ""
+        
+        eval dra_grunt_command
     else
         echo "Event: '$1' is not defined or is empty"
     fi
-    
-    echo ""
 }
+
+
+
+npm intall
+custom_cmd
 
 dra_logger
 
